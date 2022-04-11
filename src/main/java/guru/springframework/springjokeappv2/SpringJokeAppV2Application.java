1865c1a51ae0916f497a9d7c0f06b2a89d30feac
SengoMuramasaDotSrbin <biberce985@gmail.com>
2022-04-11 08:42:45 +0200

    The initial stage of the project.

---
+++ src/main/java/guru/springframework/springjokeappv2/SpringJokeAppV2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.springjokeappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokeAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/springjokeappv2/SpringJokeAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springjokeappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokeAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
